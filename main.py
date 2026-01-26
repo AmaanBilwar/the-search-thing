f5ba392bd7417ea79eaa524338a638bea3277bdc
Karthik R <karthikeya.rachamolla@gmail.com>
2026-01-26 22:56:57 +0530

    adding function to lib.rs

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def add_function(x: int, y: int) -> int:
     result = add_numbers(x, y)
     print(result)
     return result
-    
+
+
 def walk_dir(dir: str):
-    from the_search_thing import walk # ty:ignore[unresolved-import]
-    
-    walk("C:/Users/karth/Downloads")
+    from the_search_thing import walk  # ty:ignore[unresolved-import]
+
+    walk(dir)
 
 
 if __name__ == "__main__":
-    add_function(12, 13)
+    # add_function(12, 13)
+
+    walk_dir("C:/Users/karth/Downloads")
--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,6 @@ mod walk;
 #[pymodule]
 fn the_search_thing(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add::add_numbers, m)?)?;
+    m.add_function(wrap_pyfunction!(walk::walk, m)?)?;
     Ok(())
 }
