21da4249a7aab4880df61bca75263e53fb3f8b82
Karthik R <karthikeya.rachamolla@gmail.com>
2026-01-28 16:25:49 +0530

    implementing base search handler logic and axiom backend

--- electron-react-app-main/app/components/home/Home.tsx
+++ electron-react-app-main/app/components/home/Home.tsx
@@ -1,18 +1,38 @@
 import { useEffect, useState } from 'react'
 import {Searchbar} from '../ui/searchbar'
 import { Badge } from '../ui/badge'
+import { useConveyor } from '@/app/hooks/use-conveyor'
 import './styles.css'
 
 export default function Home() {
-  const [q, setQ] = useState("")
+  const [query, setQuery] = useState("")
+  const search = useConveyor("search")
+  const [results, setResults] = useState<string[]>([])
+  const [isIndexed, setIsIndexed] = useState<boolean>(false)
+  const [index, setIndexed] = useState<boolean>(false)
+  
+  const handleSearch = async () => {
+    const checkRes = await search.check(query)
+    setIsIndexed(checkRes)
+    const indexRes = await search.index(query)
+    setIndexed(indexRes)
+    const res = await search.search(query)
+    setResults(res.results)
+  }
 
   return (
     <div className="welcome-content flex flex-col gap-5">
       <div className="flex items-center h-[20%]">
         <Searchbar
-          value={q}
-          onChange={(e) => setQ(e.target.value)}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
           placeholder="Search…"
+          onKeyDown={(e) => {
+            if (e.key == 'Enter') {
+              e.preventDefault();
+              handleSearch();
+            }
+          }}
         />
       </div>
       <div className="flex justify-center items-center gap-4 opacity-50 hover:opacity-80 transition-opacity">
