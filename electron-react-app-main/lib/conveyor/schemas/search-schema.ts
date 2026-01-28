8ce216d70920dba909f05cb336732c1fce88c4e2
Karthik R <karthikeya.rachamolla@gmail.com>
2026-01-28 12:18:54 +0530

    adding search-schema

---
+++ electron-react-app-main/lib/conveyor/schemas/search-schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+export const searchIpcSchema = {
+  check: {
+    args: z.tuple([z.string()]),
+    return: z.boolean(),
+  },
+  index: {
+    args: z.tuple([z.string()]),
+    return: z.boolean(),
+  },
+  search: {
+    args: z.tuple([z.string()]),
+    return: z.object({
+      results: z.array(z.string()),
+    }),
+  },
+};
