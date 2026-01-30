3d1a482622589ccee54784041e65b7d89f78652c
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-30 17:41:57 -0500

    add vid adn helpers mods to lib to use them as crates in index

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use pyo3::prelude::*;
 
+mod helpers;
 mod index;
-
+mod vid;
 #[pymodule]
 fn the_search_thing(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(index::rust_indexer, m)?)?;
