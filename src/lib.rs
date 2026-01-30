4d84f21145e68edd5cbb7b0d2a83f3082a80aa53
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-30 17:37:47 -0500

    add indexer in lib and rmeove unused modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 use pyo3::prelude::*;
 
+mod aud;
+mod helpers;
+mod index;
 mod read_file;
+mod vid;
 mod walk;
 
 #[pymodule]
 fn the_search_thing(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(walk::walk_and_get_content, m)?)?;
+    m.add_function(wrap_pyfunction!(index::rust_indexer, m)?)?;
     Ok(())
 }
