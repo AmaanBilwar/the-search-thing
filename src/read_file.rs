be2e9ca5820f43f34876e174c5a01a6bd49a6f28
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-31 20:12:34 -0500

    update read file conent

--- src/read_file.rs
+++ src/read_file.rs
@@ -3,10 +3,8 @@ use std::fs;
 
 use pyo3::prelude::*;
 #[pyfunction]
-pub fn get_file_contents(file_path: String) {
-    let file_path = file_path;
-
-    println!("File Path: {file_path}");
-    let contents = fs::read_to_string(file_path).expect("shoulda been able to read the file");
-    println!("with text:\n{contents}")
+pub fn get_file_contents(file_path: String) -> PyResult<String> {
+    let contents = fs::read_to_string(&file_path)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{}", e)))?;
+    Ok(contents)
 }
