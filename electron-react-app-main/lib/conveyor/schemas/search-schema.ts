d67866b819f1e8119595c362aaee5c04b9f8fbb1
Karthik R <karthikeya.rachamolla@gmail.com>
2026-02-03 23:59:38 +0530

    updating schema for search endpoint

--- electron-react-app-main/lib/conveyor/schemas/search-schema.ts
+++ electron-react-app-main/lib/conveyor/schemas/search-schema.ts
@@ -7,12 +7,18 @@ export const searchIpcSchema = {
   },
   'index': {
     args: z.tuple([z.string()]), 
-    return: z.tuple([z.boolean(), z.string()]),
+    return: z.object({ success: z.boolean(), job_id: z.string() })
   },
   'search': {
     args: z.tuple([z.string()]),
     return: z.object({
-      results: z.array(z.string()),
+      success: z.boolean(),
+      files: z.array(
+        z.object({file_id:z.string(), content: z.string(), path: z.string()})
+      ),
+      videos: z.array(
+        z.object({file_id:z.string(), content: z.string(), parth: z.string()})  
+      )
     }),
   },
   // system operations
